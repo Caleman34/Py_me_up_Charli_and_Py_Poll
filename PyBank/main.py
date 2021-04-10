012b7f01d3735341c60b894105e2cd7e5b7b2376
Chris Aleman <chris.aleman34@gmail.com>
2021-04-10 11:29:17 -0500

    updating files

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,18 +2,37 @@
 import os
 import csv
 
-#create variable for path to file
-PyBank_Data = os.path.join('..', 'Resources', 'PyBank_Data.csv')
+#lists to store data
+PyBank_Data = os.path.join('Resources', 'PyBank_Data.csv')
 
 #reading csv file with specified delimiter and variable that holds contents
-with open(PyBank_Data) as csvfile:
-    PyBank_Data_reader = csv.reader(csvfile, delimiter=',')
-
-    print(PyBank_Data_reader)
-
+with open(PyBank_Data, 'r') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter =',')
+    
     #read the header row first
-    csv_header = next(PyBank_Data_reader)
-    print(f"CSV HEADER: {csv_header}")
+    csv_header = next(csv_reader)
+    
+    #create variables with empty lists
+    months = []
+    profit_losses = []
+
 
     #read through the rows of data after the header
-    for row in PyBank_Data_reader
+    for row in csv_reader:
+      #add row count to months for number of "months" in data set
+      months.append(row[0])
+      #calculate length of "totalMonths" aka total number of rows after the header
+      totalMonths = (len(months)) 
+      
+      #add profit/losses to sum list
+      profit_losses.append(row[1])
+       #sum of total profit/losses
+      total_profit_losses = sum(profit_losses)
+     
+         
+    
+    #print data results
+    print(totalMonths)
+    print(total_profit_losses)
+      
+
