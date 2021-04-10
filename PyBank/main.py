893e1613e12b83823d2e4d9bd3cc2f49a7d02fd8
Chris Aleman <chris.aleman34@gmail.com>
2021-04-10 13:20:51 -0500

    pybank solution

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,8 +17,10 @@ with open(PyBank_Data, 'r') as csvfile:
     profit_losses = []
 
 
+
     #read through the rows of data after the header
     for row in csv_reader:
+     
       #add row count to months for number of "months" in data set
       months.append(row[0])
       #calculate length of "totalMonths" aka total number of rows after the header
@@ -26,19 +28,30 @@ with open(PyBank_Data, 'r') as csvfile:
       
       #add profit/losses in column 1 to ptofit/loses variable empty list
       profit_losses.append(int(row[1]))
-       #sum of total profit/losses variable
+      #sum of total profit/losses variable
       total_profit_losses = sum(profit_losses)
-     
-         
+      
+      #calculate the average change of profit/losses for entire period
+      average_change = total_profit_losses / totalMonths
+
+      #calculate max increase
+      max_profit = max(profit_losses)
+      index_max = profit_losses.index(max_profit)
+      max_month = months[index_max]
     
+      #calculate max decrease
+      min_profit = min(profit_losses)
+      index_min = profit_losses.index(min_profit)
+      min_month = months[index_min]
+
     #create variable financial analysis for output to be printed and exported
     financial_analysis = (f'''Financial Analysis
     -----------------
     Total Months: {totalMonths}
     Total: ${total_profit_losses}
-    Average Change: 
-    Greatest increase in profits:
-    Greatest decrease in profits: ''')
+    Average Change: ${average_change: .2f}
+    Greatest increase in profits: {max_month} (${max_profit})
+    Greatest decrease in profits: {min_month} (${min_profit})''')
    
     #print out analysis
     print(financial_analysis)
